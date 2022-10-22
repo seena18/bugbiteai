51e9c65e16689ca198e9bd2925510314b8fc6ece
Seena Abed <66761673+seena18@users.noreply.github.com>
2022-10-22 12:57:40 -0700

    Add files via upload

---
+++ serpscrape.py
@@ -0,0 +1,68 @@
+import os, requests, lxml, re, json, urllib.request
+from bs4 import BeautifulSoup
+from serpapi import GoogleSearch
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
+}
+
+params = {
+    "q": "mosquito bite", # search query
+    "tbm": "isch",                # image results
+    "hl": "en",                   # language of the search
+    "gl": "us",                   # country where search comes from
+    "ijn": "0"                    # page number
+}
+
+html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
+soup = BeautifulSoup(html.text, "lxml")
+def serpapi_get_google_images():
+    image_results = []
+    
+    for query in ["mosquito bite"]:
+        # search query parameters
+        params = {
+            "engine": "google",               # search engine. Google, Bing, Yahoo, Naver, Baidu...
+            "q": query,                       # search query
+            "tbm": "isch",                    # image results
+            "num": "1000",                     # number of images per page
+            "ijn": 0,                         # page number: 0 -> first page, 1 -> second...
+            "api_key": "f6279f73878b2dda43e71adb04a87b4aac6313813ae4a03c05b65cdc3ee7bb6e"   # your serpapi api key
+            # other query parameters: hl (lang), gl (country), etc  
+        }
+    
+        search = GoogleSearch(params)         # where data extraction happens
+    
+        images_is_present = True
+        while images_is_present and params["ijn"]<1:
+            results = search.get_dict()       # JSON -> Python dictionary
+    
+            # checks for "Google hasn't returned any results for this query."
+            if "error" not in results:
+                for image in results["images_results"]:
+                    if image["original"] not in image_results:
+                        image_results.append(image["original"])
+                
+                # update to the next page
+                params["ijn"] += 1
+            else:
+                images_is_present = False
+                print(results["error"])
+    
+    # -----------------------
+    # Downloading images
+
+    for index, image in enumerate(results["images_results"], start=1):
+        print(f"Downloading {index} image...")
+        
+        opener=urllib.request.build_opener()
+        opener.addheaders=[("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36")]
+        urllib.request.install_opener(opener)
+        try:
+            urllib.request.urlretrieve(image["original"], f"./mosquito_bites/original_size_img_{index}.jpg")
+        except:
+            print(f"error saving image {index}")
+    print(json.dumps(image_results, indent=2))
+    print(len(image_results))
+
+serpapi_get_google_images()
